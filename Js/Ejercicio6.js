33e5b44783496561de864502727f2f0429cfc8d7
Santiago Lacki <santiagolacki@gmail.com>
2025-04-16 16:32:00 -0300

    Se creo el archivo Ejercicio6, se agrego el enunciado y su resolucion. Tambien se cambio el script del index

---
+++ Js/Ejercicio6.js
@@ -0,0 +1,22 @@
+
+// 6.- Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.
+// Ejemplo: 
+// input: 15 , 3
+// Output: El 15 es el número más grande
+
+document.writeln("Algoritmo para determinar que numero de los ingresados es mayor: <br>");
+
+num1 = parseInt(prompt("Ingrese el primer numero: "));
+num2 = parseInt(prompt("Ingrese el segundo numero: "));
+
+if (num1 != num2) {
+  if (num1 > num2) {
+    document.writeln(`${num1} es mayor que ${num2}`);
+  }
+  else {
+    document.writeln(`${num2} es mayor que ${num1}`);
+  }
+}
+else {
+  document.writeln(`${num1} = ${num2} <br> Los numeros son iguales.`);
+}
